b9c8f05bf0d0b11595f6cac1cfe3da3fe609f7c3
yuzhijun <664281688@qq.com>
2017-06-15 16:49:32 +0800

    Initial commit

---
+++ lartemis-api/src/main/java/com/lenovohit/lartemis_api/base/CoreActivity.java
@@ -0,0 +1,10 @@
+package com.lenovohit.lartemis_api.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * Created by yuzhijun on 2017/6/15.
+ */
+public class CoreActivity extends AppCompatActivity {
+}
---
+++ lartemis-api/src/main/java/com/lenovohit/lartemis_api/network/ApiService.java
@@ -0,0 +1,8 @@
+package com.lenovohit.lartemis_api.network;
+/**
+ * 网络接口类
+ * Created by yuzhijun on 2016/3/29.
+ */
+public interface ApiService {
+
+}
