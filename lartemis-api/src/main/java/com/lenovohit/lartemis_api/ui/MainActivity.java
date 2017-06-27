7b039f56de19ad5b90d0d8be23f1648b7c296c4b
yuzhijun <664281688@qq.com>
2017-06-27 13:29:15 +0800

    LArtemis去除application

---
+++ lartemis-api/src/main/java/com/lenovohit/lartemis_api/ui/MainActivity.java
@@ -0,0 +1,10 @@
+package com.lenovohit.lartemis_api.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yuzhijun on 2017/6/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
